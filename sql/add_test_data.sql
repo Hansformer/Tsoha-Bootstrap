6be93edfed2eb99f0442f00944c835b5e7ab26ad
Mikael Heino <heinomikael@gmail.com>
2016-02-01 21:55:29 +0200

    SQL skriptit alustettu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,17 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Asiakas (Nimimerkki, Salasana, Email, Syntymapaiva, Sukupuoli, Paikkakunta, Yllapitaja, Paritele)
+VALUES ('Killermaster', 'salainen', 'killer.master@gnail.com', DATE '1994-01-01', true, 'Tampere', false, false),
+	   ('Admin', 'admin', 'admin@adminer.biz', DATE '2001-03-12', true, 'Admintown', true, false),
+	   ('Paritele', 'admin', 'valvonta@paritele.oy', DATE '1980-05-02', false, 'Helsinki', false, true),
+	   ('CutesyKitty', 'tosisalainen', 'kittens@gnail.com', DATE '1992-02-02', false, 'Espoo', false, false);
+	   
+INSERT INTO Viesti (LahettavaID, VastaanottavaID, Sisalto, Lahetysaika)
+VALUES (3, 1, 'lopeta ahdistelu', TIMESTAMP '2016-01-01 00:01:02'),
+	   (1, 3, 'en >:)', NOW());
+	   
+INSERT INTO EsittelysivuJulkinen (AsiakasID, Sisalto)
+VALUES (1, 'Moi kiki, oon best'),
+	   (2, 'Olen ADMINKING');
+	   
+INSERT INTO  EsittelysivuSalainen (AsiakasID, KenelleID, Sisalto)
+VALUES (1, 2, 'Tätä ei nää kukaan paitsi SÄÄ ;)');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,37 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Asiakas
+(
+	AsiakasID SERIAL PRIMARY KEY,
+	Nimimerkki varchar(32) NOT NULL UNIQUE,
+	Salasana varchar(64) NOT NULL,
+	Email varchar(64) NOT NULL,
+	Syntymapaiva DATE,
+	Sukupuoli boolean,
+	Paikkakunta varchar(128) NOT NULL, 
+	Yllapitaja boolean DEFAULT FALSE,
+	Paritele boolean DEFAULT FALSE
+);
+
+CREATE TABLE Viesti
+(
+	ViestiID integer SERIAL PRIMARY KEY,
+	LahettavaID integer references Asiakas(AsiakasID),
+	VastaanottavaID integer references Asiakas(AsiakasID),
+	Sisalto varchar(512) NOT NULL,
+	Lahetysaika timestamp NOT NULL
+);
+
+CREATE TABLE EsittelysivuJulkinen
+(
+	SivuID integer SERIAL PRIMARY KEY,
+	AsiakasID integer references  Asiakas(AsiakasID),
+	Sisalto varchar(1024) NOT NULL
+);
+
+CREATE TABLE EsittelysivuSalainen
+(
+	SivuID integer SERIAL PRIMARY KEY,
+	AsiakasID integer references Asiakas(AsiakasID),
+	KenelleID integer references Asiakas(AsiakasID),
+	Sisalto varchar(256) NOT NULL
+);
